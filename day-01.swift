9efe5d1724c1eeba148a2158f45324e40c282e56
Witek Bobrowski <witek@bobrowski.com.pl>
2021-12-02 00:14:29 +0100

    Add Solution to Day 1 Part 2

--- day-01.swift
+++ day-01.swift
@@ -1,4 +1,7 @@
-import Foundation 
+// --- Day 1: Sonar Sweep ---
+// https://adventofcode.com/2021/day/1
+
+import Foundation
 
 func readInput(from path: String) -> [Int] {
     let url = URL(fileURLWithPath: path)
@@ -12,13 +15,29 @@ let input = readInput(from: "input/day-01.txt")
 
 // MARK: - Part One
 private func countNumberOfIncreases(from input: [Int]) -> Int {
-    var lastValue: Int?
-    return input.reduce(0) { sum, value in
-        defer { lastValue = value }
-        return lastValue.map { $0 < value ? sum + 1 : sum } ?? sum
+    input.indices.reduce(0) { sum, index in
+        guard input.indices.contains(index - 1) else { return sum }
+        return input[index - 1] < input[index] ? sum + 1 : sum
     }
 }
 
 print("Solution:", countNumberOfIncreases(from: input))
 // Solution: 1521
 
+print("--- Part Two ---")
+
+private func countNumberOfIncreasesSlidingWindows(from input: [Int]) -> Int {
+    input.indices.reduce(0) { sum, index in
+        guard
+            input.indices.contains(index - 3),
+            input.indices.contains(index - 2),
+            input.indices.contains(index - 1)
+        else { return sum }
+        let previous = input[index - 3] + input[index - 2] + input[index - 1]
+        let current = input[index - 2] + input[index - 1] + input[index]
+        return previous < current ? sum + 1 : sum
+    }
+}
+
+print("Solution:", countNumberOfIncreasesSlidingWindows(from: input))
+// Solution: 1543
