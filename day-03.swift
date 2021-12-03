04d6cd90e7ffb3814a64c26652ed53e514c7646f
Witek Bobrowski <witek@bobrowski.com.pl>
2021-12-03 20:29:43 +0100

    Add Solution to Day 3 Part 2

--- day-03.swift
+++ day-03.swift
@@ -6,7 +6,7 @@ import Foundation
 func readInput(from path: String) throws -> [String] {
     let url = URL(fileURLWithPath: path)
     let contents = try String(contentsOf: url, encoding: .utf8)
-    return contents.split(separator: "\n").compactMap(String.init)
+    return contents.split(separator: "\n").map(String.init)
 }
 let input = try readInput(from: "input/day-03.txt")
 
@@ -16,11 +16,9 @@ print("--- Day 3: Binary Diagnostic ---")
 private func getPowerConsumtion(from input: [String]) -> Int {
     let gamma = input.reduce([Int]()) { (count, bits) in
         if count.isEmpty { return bits.map { $0 == "0" ? 0 : 1 } }
-        return zip(count, bits).map { (count, bit) in
-            count + (bit == "0" ? 0 : 1)
-        }
-    }.map { ($0 < input.count / 2) ? "0" : "1" }.joined(separator: "")
-    let epsilon = gamma.map { $0 == "0" ? "1" : "0" }.joined(separator: "")
+        return zip(count, bits).map { $0 + (Int(String($1)) ?? 0) }
+    }.map { ($0 * 2 < input.count) ? "0" : "1" }.joined()
+    let epsilon = gamma.map { $0 == "0" ? "1" : "0" }.joined()
     return Int(strtoul(gamma, nil, 2)) * Int(strtoul(epsilon, nil, 2))
 }
 
@@ -30,3 +28,46 @@ print("Solution:", getPowerConsumtion(from: input))
 // MARK: - Part Two
 print("--- Part Two ---")
 
+private func findMostCommonBits(from input: [String]) -> String {
+    input.reduce([Int]()) { (count, bits) in
+        if count.isEmpty { return bits.map { $0 == "0" ? 0 : 1 } }
+        return zip(count, bits).map { $0 + (Int(String($1)) ?? 0) }
+    }
+    .map { ($0 * 2 < input.count) ? "0" : ($0 * 2 == input.count ? "-" : "1") }
+    .joined()
+}
+
+private func findCandidate(
+    from input: [String], leadingBit: Character, matchesMostsCommonBit: Bool
+) -> String? {
+    var candidates = input
+    var bitIndex = "".startIndex
+    while candidates.count > 1 {
+        let mostCommonBits = findMostCommonBits(from: candidates)
+        candidates = candidates.filter { candidate in
+            if mostCommonBits[bitIndex] == "-" {
+                return candidate[bitIndex] == leadingBit
+            } else if matchesMostsCommonBit {
+                return candidate[bitIndex] == mostCommonBits[bitIndex]
+            } else {
+                return candidate[bitIndex] != mostCommonBits[bitIndex]
+            }
+        }
+        bitIndex = mostCommonBits.index(after: bitIndex)
+    }
+    return candidates.first
+}
+
+private func getLifeSupportRating(from input: [String]) -> Int {
+    let oxygenGeneratorRating = findCandidate(
+        from: input, leadingBit: "1", matchesMostsCommonBit: true
+    )!
+    let co2ScrubberRating = findCandidate(
+        from: input, leadingBit: "0", matchesMostsCommonBit: false
+    )!
+
+    return Int(strtoul(oxygenGeneratorRating, nil, 2)) * Int(strtoul(co2ScrubberRating, nil, 2))
+}
+
+print("Solution:", getLifeSupportRating(from: input))
+// Solution: 6677951
